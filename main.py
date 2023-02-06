1b1a029800c0454da571a298d5a71e3b4c3c1172
oxau <notoriousact@gmail.com>
2023-02-05 23:34:43 -0800

    cgc

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    str = 'Python Programming'
+    sub1 = str[:7]
+    sub2 = str[7:18]
+    print (sub2)
+    print (sub1)
+    print (sub2 + sub1)
+    
     pass
 
 
